b66418fb9582583ed77fcb9773dfb4bf7714ee37
hansun-hub <hansun@naver.com>
2023-07-10 14:49:19 +0900

    sign up 정교화, info_edit 만듬

--- mypage/urls.py
+++ mypage/urls.py
@@ -6,4 +6,6 @@ app_name = "mypage"
 urlpatterns = [
     path('', views.mypage_view, name="mypage"),
     path("edit", views.edit_view, name="edit"),
+    path('profile/update/', views.profile_update_view, name='profile_update'),
+
 ]
